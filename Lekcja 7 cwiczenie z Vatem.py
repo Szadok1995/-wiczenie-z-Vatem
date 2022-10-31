d271c99fc0e532efea7ac407c49f601574422e82
Szadok1995 <117068953+Szadok1995@users.noreply.github.com>
2022-10-31 09:58:14 +0100

    Add files via upload

---
+++ Lekcja 7 cwiczenie z Vatem.py
@@ -0,0 +1,18 @@
+"""
+Lekcja 7
+
+** potęgowanie
+// dzielenie w dół ( zawsze zaokrągla w dół)
+%  modulo (reszta z dzielenia np. 4&3 = 1 ( bo 1 i 1/3)
+
+"""
+VAT = 21
+obliczonyVAT = (1 + VAT / 100)
+CenaNettoJava = 10
+CenaNettoAjax = 20
+
+CenaBruttoJava = CenaNettoJava * obliczonyVAT
+CenaBruttoAjax = CenaNettoAjax * obliczonyVAT
+
+
+Lekcja 8
